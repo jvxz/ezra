e5e95f251296a07017e54d210bda5edb8d022f3e
jamie 🌨️ <jamie@wisp.bio>
2025-04-30 17:08:34 -0400

    feat: add react scan

--- entrypoints/dashboard/main.tsx
+++ entrypoints/dashboard/main.tsx
@@ -1,8 +1,13 @@
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { scan } from 'react-scan'
 import App from './App.tsx'
 import '@/assets/globals.css'
+
+scan({
+    enabled: process.env.NODE_ENV === 'development',
+})
 
 const queryClient = new QueryClient()
 
@@ -11,5 +16,4 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
         <QueryClientProvider client={queryClient}>
             <App />
         </QueryClientProvider>
-    </React.StrictMode>
-)
+    </React.StrictMode>,)
