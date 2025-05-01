b042c518bdfc9ecf3b41fddd7ace809f701a6864
jamie 🌨️ <jamie@wisp.bio>
2025-05-01 14:05:56 -0400

    feat: add toaster

---
+++ components/ui/sonner.tsx
@@ -0,0 +1,23 @@
+import type { ToasterProps } from 'sonner'
+import { Toaster as Sonner } from 'sonner'
+
+function Toaster({ ...props }: ToasterProps) {
+  return (
+    <Sonner
+      toastOptions={{
+        classNames: {
+          toast:
+            'group toast !bg-card !text-card-foreground !border-border !shadow-none',
+          description: '!text-muted-foreground',
+          actionButton:
+            '!bg-primary !text-primary-foreground font-medium',
+          cancelButton:
+            '!bg-muted !text-muted-foreground font-medium',
+        },
+      }}
+      {...props}
+    />
+  )
+}
+
+export { Toaster }
