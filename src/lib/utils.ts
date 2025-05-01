f3f49df8f2d9955f382699b00fe73792c4241c62
jamie 🌨️ <jamie@wisp.bio>
2025-05-01 16:40:01 -0400

    feat: timestamp formatter helper

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,7 +1,19 @@
 import type { ClassValue } from 'clsx'
 import { clsx } from 'clsx'
+import * as d from 'date-fns-tz'
 import { twMerge } from 'tailwind-merge'
+
+const tz = Intl.DateTimeFormat().resolvedOptions().timeZone
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
+}
+
+export function formatTimestamp(timestampInMillis: number, type: 'date' | 'time') {
+  switch (type) {
+    case 'date':
+      return d.formatInTimeZone(timestampInMillis, tz, 'MMM dd, yyyy')
+    case 'time':
+      return d.formatInTimeZone(timestampInMillis, tz, 'h:mm a')
+  }
 }
