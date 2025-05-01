d4eaa0725f702bb61986180c58e3b15dacf6d006
jamie 🌨️ <jamie@wisp.bio>
2025-05-01 15:19:57 -0400

    refactor: edit devtools

--- components/dev-tools.tsx
+++ components/dev-tools.tsx
@@ -8,6 +8,7 @@ function DevTools() {
     <div className="bg-card fixed top-12 right-1/2 left-1/2 z-50 flex w-fit -translate-x-1/2 gap-2 rounded border p-2">
       <Button onClick={() => start()}>start session</Button>
       <Button onClick={() => stop()}>stop session</Button>
+      <ThemeToggle />
     </div>
   )
 }
