0404f74ed072afc6a00d31776e1e5128f6331f33
jamie 🌨️ <jamie@wisp.bio>
2025-05-08 14:31:07 -0400

    feat: make top buttons work

--- entrypoints/popup/components/top-buttons.tsx
+++ entrypoints/popup/components/top-buttons.tsx
@@ -1,16 +1,58 @@
 import { Button } from '@/components/ui/button'
+import { useCurrentSession } from '@/lib/hooks/use-current-session'
+import { useStatus } from '@/lib/hooks/use-status'
 
 function TopButtons() {
   return (
     <div className="flex items-center gap-2 *:flex-1">
-      <Button>
-        Stop
-      </Button>
-      <Button>
-        Dashboard
-      </Button>
+      <StopButton />
+      <DashboardButton />
     </div>
   )
 }
 
+function StopButton() {
+  const { start, stop } = useCurrentSession()
+  const { data: status } = useStatus()
+
+  const handleClick = () => {
+    if (status?.session === true) {
+      stop()
+    }
+    else {
+      start()
+    }
+  }
+
+  return (
+    <Button onClick={handleClick}>
+      {status?.session === true
+        ? (
+          <>
+            Stop
+            <span className="iconify icon-[ph--stop-fill]"></span>
+          </>
+        )
+        : (
+          <>
+            Start
+            <span className="iconify icon-[ph--play-fill]"></span>
+          </>
+        )}
+    </Button>
+  )
+}
+
+function DashboardButton() {
+  const handleClick = () => {
+    const url = browser.runtime.getURL('/dashboard.html')
+    window.open(url, '_blank')
+  }
+
+  return (
+    <Button onClick={handleClick}>
+      Dashboard
+    </Button>
+  )
+}
 export { TopButtons }
--- entrypoints/popup/main.tsx
+++ entrypoints/popup/main.tsx
@@ -1,5 +1,7 @@
 /* eslint-disable style/function-paren-newline style/comma-dangle style/indent */
 
+import { createIDBPersister, queryClient } from '@/lib/query-client.ts'
+import { PersistQueryClientProvider } from '@tanstack/react-query-persist-client'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
@@ -7,6 +9,13 @@ import '@/assets/globals.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
     <React.StrictMode>
-        <App />
-    </React.StrictMode>
-)
+        <PersistQueryClientProvider
+            client={queryClient}
+            persistOptions={{
+                persister: createIDBPersister(),
+                maxAge: 1000 * 60 * 60 * 24,
+            }}
+        >
+            <App />
+        </PersistQueryClientProvider>
+    </React.StrictMode>,)
