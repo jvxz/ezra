fb4c6a6e59cf22184bb333e50a6c6f77a7016fe8
jamie 🌨️ <jamie@wisp.bio>
2025-05-16 18:18:35 -0400

    feat: update current session data msg action

--- lib/messages/get-current-session-data.ts
+++ lib/messages/get-current-session-data.ts
@@ -1,6 +1,6 @@
-import { sessionStorage } from '@/lib/storage/sessions'
-import { taskStorage } from '@/lib/storage/tasks'
-import { calcEfficiency } from '@/lib/utils'
+import type { ConvexClient } from 'convex/browser'
+import { api } from '@/convex/_generated/api'
+import { calcTaskTotals } from '@/lib/utils'
 import { Data, Effect } from 'effect'
 
 class GetLiveDataError extends Data.TaggedError('GetLiveDataError')<{
@@ -8,81 +8,50 @@ class GetLiveDataError extends Data.TaggedError('GetLiveDataError')<{
   message?: string
 }> {}
 
-const program = Effect.gen(function* (_) {
-  const currentSession = yield* Effect.tryPromise({
-    try: async () => sessionStorage.getValue(),
-    catch: e => new GetLiveDataError({
-      cause: JSON.stringify(e),
-      message: 'Failed to get sessions from local storage',
-    }),
-  })
+function program(convex: ConvexClient, rate: number) {
+  return Effect.gen(function* (_) {
+    const currentSession = yield* Effect.tryPromise({
+      try: async () => convex.query(api.sessions.pickCurrentSessionWithTasks, {
+      }),
+      catch: e => new GetLiveDataError({
+        cause: JSON.stringify(e),
+        message: 'Failed to get sessions from local storage',
+      }),
+    })
 
-  if (!currentSession) return null
+    if (!currentSession.session) return null
 
-  const currentTask = yield* _(Effect.tryPromise({
-    try: async () => taskStorage.getValue(),
-    catch: e => new GetLiveDataError({
-      cause: JSON.stringify(e),
-      message: 'Failed to get tasks from local storage',
-    }),
-  }))
+    const currentTask = yield* Effect.tryPromise({
+      try: async () => convex.query(api.tasks.pickCurrentTask, {
+      }),
+      catch: e => new GetLiveDataError({
+        cause: JSON.stringify(e),
+        message: 'Failed to get current task',
+      }),
+    })
 
-  if (!currentTask) {
-    return {
-      duration: currentSession.duration,
-      efficiency: currentSession.efficiency,
-      earnings: currentSession.earnings,
-      taskCount: currentSession.tasks.length,
-      isActive: currentSession.end === 'Active',
+    if (!currentTask) {
+      const taskTotals = calcTaskTotals(currentSession.tasks, rate)
+
+      return {
+        sessionData: currentSession,
+        taskData: taskTotals,
+        currentTaskData: null,
+      }
     }
-  }
 
-  const dur = currentTask.duration + currentSession.duration
-  const aet = currentTask.aet + currentSession.tasks.reduce((a, c) => a + c.aet, 0)
-  const eff = calcEfficiency(dur, aet)
-  const earn = currentTask.earnings + currentSession.earnings
+    const taskTotals = calcTaskTotals([...currentSession.tasks, currentTask], rate)
 
-  const data = {
-    duration: dur,
-    efficiency: eff,
-    earnings: earn,
-    taskCount: currentSession.tasks.length,
-    isActive: currentSession.end === 'Active',
-  }
+    const data = {
+      sessionData: currentSession,
+      taskData: taskTotals,
+      currentTaskData: currentTask,
+    }
 
-  // const allSessions = yield* _(Effect.tryPromise({
-  //   try: async () => db.sessions.toArray(),
-  //   catch: e => new GetLiveDataError({
-  //     cause: JSON.stringify(e),
-  //     message: 'Failed to get sessions from db',
-  //   }),
-  // }), Effect.map((s) => {
-  //   const dur = s.reduce((acc, curr) => acc + curr.duration, 0)
-  //   const eff = s.reduce((acc, curr) => acc + curr.efficiency, 0) / s.length
-  //   const earn = s.reduce((acc, curr) => acc + curr.earnings, 0)
+    return data
+  })
+}
 
-  //   if (!currentSession) {
-  //     return {
-  //       duration: dur,
-  //       efficiency: eff,
-  //       earnings: earn,
-  //       taskCount: s.length,
-  //       isActive: false,
-  //     }
-  //   }
-
-  //   return {
-  //     duration: dur + currentSession.duration,
-  //     efficiency: eff + currentSession.efficiency,
-  //     earnings: earn + currentSession.earnings,
-  //     taskCount: s.length + currentSession.tasks.length,
-  //     isActive: true,
-  //   }
-  // }))
-
-  return data
-})
-
-export async function getCurrentSessionData() {
-  return program.pipe(Effect.runPromise)
+export async function getCurrentSessionData(convex: ConvexClient, rate: number) {
+  return program(convex, rate).pipe(Effect.runPromise)
 }
