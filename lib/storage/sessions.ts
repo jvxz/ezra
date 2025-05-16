81e3125aeb1ad5ae110ac6ee04cb1d74a4c0a72c
jamie 🌨️ <jamie@wisp.bio>
2025-05-16 18:23:50 -0400

    refactor: remove old implementations

--- lib/storage/sessions.ts
+++ lib/storage/sessions.ts
@@ -1,14 +1,6 @@
-import type { Task } from './tasks'
-
 export interface Session {
   id: string
   description: `${number} tasks`
   start: number
   end: number | 'Active'
-  duration: number
-  efficiency: number
-  earnings: number
-  tasks: Task[]
 }
-
-export const sessionStorage = storage.defineItem<Session>('local:session')
