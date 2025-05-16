cb846225f1989c66567418bb4620761c6351589f
jamie 🌨️ <jamie@wisp.bio>
2025-05-16 18:18:52 -0400

    feat: update start session msg action

--- lib/messages/handle-start-session.ts
+++ lib/messages/handle-start-session.ts
@@ -1,5 +1,6 @@
 import type { Session } from '@/lib/storage/sessions'
-import { sessionStorage } from '@/lib/storage/sessions'
+import type { ConvexClient } from 'convex/browser'
+import { api } from '@/convex/_generated/api'
 import { Data, Effect } from 'effect'
 import { create } from 'mutative'
 import { statusStorage } from '../storage/status'
@@ -9,53 +10,53 @@ class StartSessionError extends Data.TaggedError('StartSessionError')<{
   message?: string
 }> {}
 
-const program = Effect.gen(function* () {
-  const status = yield* Effect.tryPromise({
-    try: async () => statusStorage.getValue(),
-    catch: e => new StartSessionError({
-      cause: JSON.stringify(e),
-      message: 'Failed to get status storage',
-    }),
-  })
-
-  if (status.session) {
-    throw new StartSessionError({
-      message: 'Session already active',
+function program(convex: ConvexClient) {
+  return Effect.gen(function* () {
+    const status = yield* Effect.tryPromise({
+      try: async () => statusStorage.getValue(),
+      catch: e => new StartSessionError({
+        cause: JSON.stringify(e),
+        message: 'Failed to get status storage',
+      }),
     })
-  }
 
-  yield* Effect.tryPromise({
-    try: async () => statusStorage.setValue(create(status, (draft) => {
-      draft.session = true
-    })),
-    catch: e => new StartSessionError({
-      cause: JSON.stringify(e),
-      message: 'Failed to set status storage',
-    }),
+    if (status.session) {
+      throw new StartSessionError({
+        message: 'Session already active',
+      })
+    }
+
+    yield* Effect.tryPromise({
+      try: async () => statusStorage.setValue(create(status, (draft) => {
+        draft.session = true
+      })),
+      catch: e => new StartSessionError({
+        cause: JSON.stringify(e),
+        message: 'Failed to set status storage',
+      }),
+    })
+
+    const draft: Session = {
+      id: crypto.randomUUID(),
+      description: '0 tasks',
+      start: Date.now(),
+      end: 'Active',
+    }
+
+    yield* Effect.log('Starting session')
+
+    yield* Effect.tryPromise({
+      try: async () => convex.mutation(api.sessions.create, draft),
+      catch: e => new StartSessionError({
+        cause: JSON.stringify(e),
+        message: 'Failed to start session',
+      }),
+    })
+
+    return draft
   })
+}
 
-  const draft: Session = {
-    id: crypto.randomUUID(),
-    description: '0 tasks',
-    start: Date.now(),
-    end: 'Active',
-    duration: 0,
-    earnings: 0,
-    efficiency: 0,
-    tasks: [],
-  }
-
-  yield* Effect.tryPromise({
-    try: async () => sessionStorage.setValue(draft),
-    catch: e => new StartSessionError({
-      cause: JSON.stringify(e),
-      message: 'Failed to start session',
-    }),
-  })
-
-  return draft
-})
-
-export async function handleStartSession() {
-  return program.pipe(Effect.runPromise)
+export async function handleStartSession(convex: ConvexClient) {
+  return program(convex).pipe(Effect.runPromise)
 }
